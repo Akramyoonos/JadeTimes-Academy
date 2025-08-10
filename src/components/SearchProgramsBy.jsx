108f9327d213ed5c8ea735f0e03c21b9d8308bdc
Mohamed Azmy Mohamed Akram <Akramyoonos006@gmail.com>
2025-08-10 13:40:46 +0530

    log 7

--- src/components/SearchProgramsBy.jsx
+++ src/components/SearchProgramsBy.jsx
@@ -65,7 +65,7 @@ const SearchProgramsBy = () => {
   };
 
   return (
-    <div className="font-sans bg-gray-50">
+    <div className="font-sans bg-gray-50 px-4 sm:px-6 lg:px-8">
       <div className="bg-white shadow-lg">
         <div className="container mx-auto flex flex-col md:flex-row items-center">
           <div className="flex-shrink-0 px-6 py-4 text-gray-800 text-lg font-bold whitespace-nowrap w-full md:w-auto">
